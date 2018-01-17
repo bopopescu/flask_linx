81bc68eec2ab2941676db2d93d2ebe95cd057b3a
Gadzhik <gkurban@outlook.com>
2018-01-17 15:36:47 +0300

    commit all project file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='vsearch',
+    version='1.0',
+    description='The head first python search tools',
+    author='I',
+    author_email='1@mail.ru',
+    url='google.com',
+    py_modules=['vsearch'],
+)
